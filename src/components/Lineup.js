a44a2555b1dd6806c499501d7db69b1c5253e0b0
Phineas Ziegler <phinziegler@gmail.com>
2023-03-15 21:56:37 -0400

    2 more lineups

--- src/components/Lineup.js
+++ src/components/Lineup.js
@@ -7,6 +7,7 @@ export default class Lineup extends React.Component {
         super(props);
         this.state = {
             open: false,
+            manualShowLoc: true
         }
     }
 
@@ -14,11 +15,11 @@ export default class Lineup extends React.Component {
         return (
             <div className={"my-3 d-flex flex-row w-100 " + this.props.className}>
                 <div className="lineup-thumbnail pe-2">
-                    <img onClick={() => this.setState({ open: !this.state.open, showloc: true })} className="img-fluid" src={this.props.resultUrl} />
+                    <img onClick={() => this.setState({ open: !this.state.open, manualShowLoc: true })} className="img-fluid" src={this.props.resultUrl} />
                     {this.state.open && <p>{this.props.description}</p>}
                 </div>
                 {this.state.open && <div className="lineup-reference d-flex flex-column">
-                    {this.props.showloc() && <img onClick={() => this.setState({showloc: false})} className="lineup-reference" src={this.props.locationUrl} />}
+                    {(this.state.manualShowLoc && this.props.showloc()) && <img onClick={() => this.setState({manualShowLoc: false})} className="lineup-reference" src={this.props.locationUrl} />}
                     <img className="lineup-reference" src={this.props.lineupUrl} />
                 </div>}
             </div>
--- src/components/Viper.js
+++ src/components/Viper.js
@@ -39,12 +39,25 @@ export default class Viper extends React.Component {
                         lineupUrl="https://raw.githubusercontent.com/phinziegler/valorant-lineups/master/public/Maps/Icebox/Viper/Attack/Cat%20and%20backsite%20smoke%20lineup.png"
                         description="Run, jump, and left-click. This smoke covers vision for anyone on cat, and blocks some backsite angles." 
                         showloc={() => this.state.showLocation} />
+                    <Lineup
+                        resultUrl="https://raw.githubusercontent.com/phinziegler/valorant-lineups/master/public/Maps/Icebox/Viper/Attack/Mid%20Smoke%20Result.png"
+                        locationUrl="https://raw.githubusercontent.com/phinziegler/valorant-lineups/master/public/Maps/Icebox/Viper/Attack/Mid%20Smoke%20Loc.png"
+                        lineupUrl="https://raw.githubusercontent.com/phinziegler/valorant-lineups/master/public/Maps/Icebox/Viper/Attack/Mid%20Smoke%20Lineup.jpg"
+                        description="Standing left-click. Smokes off vision from the rafters and under kitchen."
+                        showloc={() => this.state.showLocation} />
+
                     <h2>Defense</h2>
                     <Lineup
                         resultUrl="https://raw.githubusercontent.com/phinziegler/valorant-lineups/master/public/Maps/Icebox/Viper/Defend/Defense%20Wall.png"
                         locationUrl="https://raw.githubusercontent.com/phinziegler/valorant-lineups/master/public/Maps/Icebox/Viper/Defend/Defense%20Wall%20P1.png"
                         lineupUrl="https://raw.githubusercontent.com/phinziegler/valorant-lineups/master/public/Maps/Icebox/Viper/Defend/Defense%20Wall%20P2.jpg"
                         description="Walls off the entrance to B site, and makes post plant harder for attackers."
+                        showloc={() => this.state.showLocation} />
+                    <Lineup
+                        resultUrl="https://raw.githubusercontent.com/phinziegler/valorant-lineups/master/public/Maps/Icebox/Viper/Defend/B%20One%20way%20Result.png"
+                        locationUrl="https://raw.githubusercontent.com/phinziegler/valorant-lineups/master/public/Maps/Icebox/Viper/Defend/B%20One%20Way%20(P1).png"
+                        lineupUrl="https://raw.githubusercontent.com/phinziegler/valorant-lineups/master/public/Maps/Icebox/Viper/Defend/B%20One%20Way%20(P2%2C%20Jump%20then%20right%20click%20quickly).png"
+                        description="Jump and right-click at the same time. Creates a one way on attackers moving towards yellow or site."
                         showloc={() => this.state.showLocation} />
                 </ul>
             </div>
