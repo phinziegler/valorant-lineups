952a6653720d0f2bb14b2a37cf6b536d482f66c3
Phineas Ziegler <phinziegler@gmail.com>
2023-03-15 21:03:36 -0400

    more images

--- src/components/Lineup.js
+++ src/components/Lineup.js
@@ -2,17 +2,15 @@ import React from "react";
 
 export default class Lineup extends React.Component {
 
-    /** @param {{resultUrl: string, locationUrl: string, lineupUrl: string, description: string, showloc: boolean}} props */
+    /** @param {{resultUrl: string, locationUrl: string, lineupUrl: string, description: string, showloc: *}} props */
     constructor(props) {
         super(props);
         this.state = {
             open: false,
-            showloc: props.showloc,
         }
     }
 
     render() {
-        console.log(this.state.open);
         return (
             <div className={"my-3 d-flex flex-row w-100 " + this.props.className}>
                 <div className="lineup-thumbnail pe-2">
@@ -20,7 +18,7 @@ export default class Lineup extends React.Component {
                     {this.state.open && <p>{this.props.description}</p>}
                 </div>
                 {this.state.open && <div className="lineup-reference d-flex flex-column">
-                    {this.state.showloc && <img onClick={() => this.setState({showloc: false})} className="lineup-reference" src={this.props.locationUrl} />}
+                    {this.props.showloc() && <img onClick={() => this.setState({showloc: false})} className="lineup-reference" src={this.props.locationUrl} />}
                     <img className="lineup-reference" src={this.props.lineupUrl} />
                 </div>}
             </div>
--- src/components/Viper.js
+++ src/components/Viper.js
@@ -5,6 +5,9 @@ import Lineup from "./Lineup";
 export default class Viper extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            showLocation: true
+        }
     }
 
     renderMap() {
@@ -18,33 +21,42 @@ export default class Viper extends React.Component {
         return (
             <div>
                 <h1 className="text-center">{"Viper - " + this.props.map}</h1>
+                <label>
+                    Show locations
+                    <input className="mx-1" onChange={() => this.setState({showLocation: !this.state.showLocation})} checked={this.state.showLocation} type="checkbox" />
+                </label>
                 <ul className="list-group darkened p-5">
                     <h2>Attack</h2>
                     <Lineup
                         resultUrl="https://raw.githubusercontent.com/phinziegler/valorant-lineups/master/public/Maps/Icebox/Viper/Attack/Default%20Molly%20A.png"
                         locationUrl="https://raw.githubusercontent.com/phinziegler/valorant-lineups/master/public/Maps/Icebox/Viper/Attack/A%20Default%20Lineup%20(P1).png"
                         lineupUrl="https://raw.githubusercontent.com/phinziegler/valorant-lineups/master/public/Maps/Icebox/Viper/Attack/A%20Default%20Lineup(P2).png"
-                        description="Shoot without moving. Afterwards you can run up the ramp to peak just as the molly applies vulnerability." />
+                        description="Shoot without moving. Afterwards you can run up the ramp to peak just as the molly applies vulnerability." 
+                        showloc={() => this.state.showLocation} />
                     <Lineup
                         resultUrl="https://raw.githubusercontent.com/phinziegler/valorant-lineups/master/public/Maps/Icebox/Viper/Attack/Cat%20and%20Backsite%20smoke.png"
                         locationUrl="https://raw.githubusercontent.com/phinziegler/valorant-lineups/master/public/Maps/Icebox/Viper/Attack/A%20Default%20Lineup%20(P1).png"
                         lineupUrl="https://raw.githubusercontent.com/phinziegler/valorant-lineups/master/public/Maps/Icebox/Viper/Attack/Cat%20and%20backsite%20smoke%20lineup.png"
-                        description="Run, jump, and left-click. This smoke covers vision for anyone on cat, and blocks some backsite angles." />
+                        description="Run, jump, and left-click. This smoke covers vision for anyone on cat, and blocks some backsite angles." 
+                        showloc={() => this.state.showLocation} />
                     <h2>Defense</h2>
                     <Lineup
                         resultUrl="https://raw.githubusercontent.com/phinziegler/valorant-lineups/master/public/Maps/Icebox/Viper/Defend/Defense%20Wall.png"
                         locationUrl="https://raw.githubusercontent.com/phinziegler/valorant-lineups/master/public/Maps/Icebox/Viper/Defend/Defense%20Wall%20P1.png"
-                        lineupUrl="https://raw.githubusercontent.com/phinziegler/valorant-lineups/master/public/Maps/Icebox/Viper/Defend/Defense%20Wall%20P2.png"
-                        description="Walls off the entrance to B site, and makes post plant harder for attackers." />
+                        lineupUrl="https://raw.githubusercontent.com/phinziegler/valorant-lineups/master/public/Maps/Icebox/Viper/Defend/Defense%20Wall%20P2.jpg"
+                        description="Walls off the entrance to B site, and makes post plant harder for attackers."
+                        showloc={() => this.state.showLocation} />
                 </ul>
-
-
             </div>
 
         )
     }
 
     render() {
-        return this.renderMap();
+        return (
+            <div>
+                {this.renderMap()}
+            </div>
+        );
     }
 }
